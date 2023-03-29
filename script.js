9df07e47ac8163d432647846a4c6a69ce057f32a
aditya <aditya32ft@gmail.com>
2023-03-29 11:57:10 +0530

    mobile navigation complate

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 const middleBar = document.querySelector('.middle')
 const bottomBar = document.querySelector('.bottom')
 const hamburgurIcon = document.querySelector('.hamburgur')
+const primaryNav = document.querySelector('.primary__nav')
 
 
-hamburgurIcon.addEventListener('click', ()=>{
+document.addEventListener('click', (e)=>{
+
+    if(e.target.id === 'hamburgur' || e.target.classList[0] === 'menu-bars'){
+        if(primaryNav.getAttribute(['aria-expanded']) == 'false'){
+            showNav()
+        }
+        else {
+            hideNav()
+        }
+    }
+
+    else if(e.target.id != 'primary__nav' && primaryNav.getAttribute(['aria-expanded']) == 'true'){
+        hideNav()
+    }
+})
+
+function showNav(){
     middleBar.classList.toggle('middle-animation')
     bottomBar.classList.toggle('bottom-animation')
-
-})
+    primaryNav.classList.toggle('nav-show')
+    primaryNav.setAttribute('aria-expanded', 'true')
+}
+function hideNav(){
+    middleBar.classList.remove('middle-animation')
+    bottomBar.classList.remove('bottom-animation')
+    primaryNav.classList.remove('nav-show')
+    primaryNav.setAttribute('aria-expanded', 'false')
+}
